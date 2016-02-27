82a916b25c7531d036a56afbd9f5a34fe69d585e
qianfeng <qianfeng@ghcccccccccccc.local>
2016-02-27 18:29:26 +0800

    新建项目

---
+++ ghcDemo/ghcDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ghcDemo
+//
+//  Created by qianfeng on 16/2/27.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ghcDemo/ghcDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ghcDemo
+//
+//  Created by qianfeng on 16/2/27.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
